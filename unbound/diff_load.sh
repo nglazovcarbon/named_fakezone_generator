2940121e467a1f3a63c97daac7d97844186d31a6
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-12-09 17:00:12 +0600

    Update diff_load.sh

--- unbound/diff_load.sh
+++ unbound/diff_load.sh
@@ -15,5 +15,9 @@ NEW_DOMAINS=/tmp/new_domains
 
 grep "redirect$" "$current_config" | awk '{print $2}' | tr -d '"' | LANG= sort -u > $CURRENT_DOMAINS
 $fakezoneroot/generate_unbound_configs.sh /tmp/reductor.https.resolv 127.0.0.1 process_list < $new_list | LANG= sort -u > $NEW_DOMAINS 
+# in case of old join/BSD, join: the -a and -v options are mutually exclusive
+# comment current 2 lines and uncomment following 2 lines
+# LANG= join -v1 $CURRENT_DOMAINS $NEW_DOMAINS | $fakezoneroot/unblock.sh
+# LANG= join -v1 $NEW_DOMAINS $CURRENT_DOMAINS | $fakezoneroot/block.sh "$ip"
 LANG= join -a1 -v1 -j1 $CURRENT_DOMAINS $NEW_DOMAINS | $fakezoneroot/unblock.sh
 LANG= join -a1 -v1 -j1 $NEW_DOMAINS $CURRENT_DOMAINS | $fakezoneroot/block.sh "$ip"
